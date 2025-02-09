e7f890814a22a4286418833ce4efb55b8486911e
whyKVD <faccincani.al@gmail.com>
2025-02-10 00:15:53 +0100

    provando a far funzionare tutto

--- FrontEnd/src/app/page.js
+++ FrontEnd/src/app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
         method: "GET",
         redirect: "follow"
       };
-      console.log(process.env.API_URL) 
+      console.log(process.env) 
       fetch(`${process.env.API_URL}/user/init`, requestOptions)
         .then((response) => response.text())
         .then((result) => {
